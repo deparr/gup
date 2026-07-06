869c697574711310f5f88a8ac72772c7d9efdb0e
deparr <dparrott@protonmail.com>
2026-07-06 13:30:59 -0600

    4.7 stable

--- build.zig
+++ build.zig
@@ -16,7 +16,7 @@ pub fn build(b: *std.Build) void {
     const options = b.addOptions();
     // this should be fetched at build time
     // or is this even a good idea at all?
-    options.addOption([]const u8, "latest_stable", "4.6.2");
+    options.addOption([]const u8, "latest_stable", "4.7");
     options.addOption([]const u8, "download_url", "https://downloads.godotengine.org/");
     exe.root_module.addOptions("godot_rev", options);
 
