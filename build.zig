55d3d99a8ab70079c33f34114a74dcc02443f616
deparr <dparrott@protonmail.com>
2026-08-10 11:30:46 -0600

    clean up build.zig

--- build.zig
+++ build.zig
@@ -13,14 +13,7 @@ pub fn build(b: *std.Build) void {
         }),
     });
 
-    // const latest_stable = b.option(
-    //     []const u8,
-    //     "latest-stable",
-    //     "Set default version to fetch",
-    // ) orelse "4.7.1";
-
     const options = b.addOptions();
-    // options.addOption([]const u8, "latest_stable", latest_stable);
     options.addOption([]const u8, "godotorg", "https://downloads.godotengine.org");
     options.addOption([]const u8, "github", "https://github.com/godotengine/godot/releases/download");
     exe.root_module.addOptions("download_urls", options);
